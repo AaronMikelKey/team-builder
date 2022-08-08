4870a4a722d4e50e009e12e68507b78dd837e493
Aaron Key <AaronMikelKey@gmail.com>
2022-08-08 10:20:49 -0400

    Add manager input questions

--- src/js/questions.js
+++ src/js/questions.js
@@ -1,5 +1,23 @@
-const questions = [
-
+export const questions = [
+	{
+		type: 'input',
+		name: 'managerName',
+		message: `What is your team Manager's name?`
+	},
+	{
+		type: 'input',
+		name: 'managerId',
+		message: `What is your team Manager's employee ID?`
+	},
+	{
+		type: 'input',
+		name: 'managerEmail',
+		message: `What is your team Manager's email address?`
+	},
+	{
+		type: 'input',
+		name: 'managerOffice',
+		message: `What is your team Manager's office number?`
+	},
+	
 ]
-
-export default questions
